009aaaaa1d98c7034971124f604a8a3f879dbba7
Ramu Gurrapu <ramu@techslatelimited.com>
2023-11-17 23:05:21 +0000

    renamed a local var

--- azure/storageaccount/st_locals.tf
+++ azure/storageaccount/st_locals.tf
@@ -1,4 +1,4 @@
 locals {
   prefix  = "st"
-  st_name = "${local.prefix}-${var.environment}-${var.main_project}-${var.sub_project}"
+  name = "${local.prefix}-${var.environment}-${var.main_project}-${var.sub_project}"
 }
--- azure/storageaccount/st_main.tf
+++ azure/storageaccount/st_main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_account" "storage" {
-  name                     = local.st_name
+  name                     = local.name
   resource_group_name      = var.resource_group_name
   location                 = var.location
   account_tier             = var.account_tier

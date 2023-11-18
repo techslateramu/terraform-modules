fa45e8299b1805f693afd85882436592f0c00a95
Ramu Gurrapu <ramu@techslatelimited.com>
2023-11-18 23:30:28 +0000

    added ains

--- azure/appinsights/ains-local.tf
+++ azure/appinsights/ains-local.tf
@@ -1,4 +1,4 @@
 locals {
   prefix = "ains"
-  name   = "${local.prefix}x-${var.environment}-${var.main_project}-${var.sub_project}"
+  name   = "${local.prefix}-${var.environment}-${var.main_project}-${var.sub_project}"
 }

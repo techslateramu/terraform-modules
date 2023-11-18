6c59df78dcfc1dddcdefb0b443f9aa0cf14b097a
Ramu Gurrapu <ramu@techslatelimited.com>
2023-11-18 16:20:57 +0000

    resource_group_name

--- azure/functionapp/fun_main.tf
+++ azure/functionapp/fun_main.tf
@@ -1,7 +1,7 @@
 resource "azurerm_function_app" "example" {
   name                       = local.name
   location                   = var.location
-  resource_group_name        = var.rg_name
+  resource_group_name        = var.resource_group_name
   app_service_plan_id        = var.app_service_plan_id
   storage_account_name       = var.storage_account_name
   storage_account_access_key = var.storage_account_access_key
